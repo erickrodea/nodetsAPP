4d0eb8c2b2f2d15211eacf07eaae5351a83c9422
Erick Rodea <rodeaerick@yahoo.com>
2023-12-02 17:42:27 -0500

    comments

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,70 +1,70 @@
-const chalk = require('chalk')
-const yargs = require('yargs')
-const notes = require('./notes.js')
+const chalk = require('chalk'); // Library for colorful console logs
+const yargs = require('yargs'); // Library for parsing command-line arguments
+const notes = require('./notes.js'); // Importing the notes module
 
 // Customize yargs version
-yargs.version('1.1.0')
+yargs.version('1.1.0'); // Setting the version for the command-line tool
 
 // Create add command
 yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
-    builder: {
+    command: 'add', // Command name
+    describe: 'Add a new note', // Description of the command
+    builder: { // Configuration for the command arguments
         title: {
             describe: 'Note title',
-            demandOption: true,
+            demandOption: true, // The title is required
             type: 'string'
         },
         body: {
             describe: 'Note body',
-            demandOption: true,
+            demandOption: true, // The body is required
             type: 'string'
         }
     },
-    handler(argv) {
-        notes.addNote(argv.title, argv.body)
+    handler(argv) { // Function to be executed when the command is called
+        notes.addNote(argv.title, argv.body); // Call the addNote function from the notes module
     }
 })
 
 // Create remove command
 yargs.command({
-    command: 'remove',
-    describe: 'Remove a note',
-    builder: {
+    command: 'remove', // Command name
+    describe: 'Remove a note', // Description of the command
+    builder: { // Configuration for the command arguments
         title: {
             describe: 'Note title',
-            demandOption: true,
+            demandOption: true, // The title is required
             type: 'string'
         }
     },
-    handler(argv) {
-        notes.removeNote(argv.title)
+    handler(argv) { // Function to be executed when the command is called
+        notes.removeNote(argv.title); // Call the removeNote function from the notes module
     }
 })
 
 // Create list command
 yargs.command({
-    command: 'list',
-    describe: 'List your notes',
-    handler() {
-        notes.listNotes()
+    command: 'list', // Command name
+    describe: 'List your notes', // Description of the command
+    handler() { // Function to be executed when the command is called
+        notes.listNotes(); // Call the listNotes function from the notes module
     }
 })
 
 // Create read command
 yargs.command({
-    command: 'read',
-    describe: 'Read a note',
-    builder: {
+    command: 'read', // Command name
+    describe: 'Read a note', // Description of the command
+    builder: { // Configuration for the command arguments
         title: {
             describe: 'Note title',
-            demandOption: true,
+            demandOption: true, // The title is required
             type: 'string'
         }
     },
-    handler(argv) {
-        notes.readNote(argv.title)
+    handler(argv) { // Function to be executed when the command is called
+        notes.readNote(argv.title); // Call the readNote function from the notes module
     }
 })
 
-yargs.parse()
+yargs.parse(); // Parse the command-line arguments and execute the corresponding command
